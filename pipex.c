f13fd8c954a824db9a76b5c4649b0662302fb53f
Shadow-er <mlakhssa@student.1337.ma>
2022-01-31 16:03:42 +0100

    go

--- pipex.c
+++ pipex.c
@@ -1,48 +1,137 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlakhssa <mlakhssa@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/31 14:56:43 by mlakhssa          #+#    #+#             */
+/*   Updated: 2022/01/31 16:03:17 by mlakhssa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 #include<stdio.h>
+#include <stdlib.h>
 #include<fcntl.h>
 #include<sys/wait.h>
 
-int main(int argc,char *argv[])
+char	**free_problem(char **arg, int position)
 {
-    int fd[2];
-    int file;
-    int pid;
-    if (pipe(fd) == -1)
-        return 1;
-    pid = fork();
-    if(pid == 0)
-    {
-        file = open(argv[1],O_RDONLY);
-        if (file == -1)
-            return (-1);
-        close(fd[0]);
-        dup2(file,0);
-        dup2(fd[1],1);
-        char *arg[] = { "bash", "-c", argv[2], NULL };
-        execve("/usr/bin/bash", arg,NULL);
-        perror("execve");
-        close(file);
-        close(fd[1]);
-        // char *s;
-        // s = get_next_line(file);
-    }
-    else
-    {
-        wait(NULL);
-        int file2 = open(argv[4],O_WRONLY | O_CREAT );
-        if (file2 == -1)
-            return (-1);
-        close(fd[1]);
-        dup2(fd[0],0);
-        dup2(file2,1);
-        char *arg[] = { "bash", "-c", argv[3], NULL };
-        execve("/usr/bin/bash", arg,NULL);
-        perror("execve");
-        close(fd[0]);
-        
-        close(file2);
-    }
-    close(fd[0]);
-    close(fd[1]);
+	int	i;
+
+	i = 0;
+	while (i < position)
+	{
+		free(arg[i]);
+		i++;
+	}
+	free(arg);
+	return (0);
+}
+
+char	**alloc(char **arg, char *argv)
+{
+	int	i;
+
+	i = 0;
+	while (argv[i])
+		i++;
+	arg = (char **)malloc(sizeof(char *) * 4);
+	if (arg == 0)
+		return (0);
+	arg[0] = (char *)malloc(sizeof(char) * 5);
+	if (arg[0] == 0)
+		return (free_problem(arg, 0));
+	arg[1] = (char *)malloc(sizeof(char) * 3);
+	if (arg[1] == 0)
+		return (free_problem(arg, 1));
+	arg[2] = (char *)malloc(sizeof(char) * (i + 1));
+	if (arg[2] == 0)
+		return (free_problem(arg, 2));
+	arg[0] = "bash";
+	arg[1] = "-c";
+	arg[2] = argv;
+	arg[3] = NULL;
+	return (arg);
+}
+
+int	part_1(char **argv, int fd[2])
+{
+	char	**arg;
+	int		file;
+
+	arg = NULL;
+	arg = alloc(arg, argv[2]);
+	file = open(argv[1], O_RDONLY);
+	if (file == -1)
+	{
+		perror(argv[1]);
+	}
+	close(fd[0]);
+	dup2(file, 0);
+	dup2(fd[1], 1);
+	execve("/bin/bash", arg, NULL);
+	perror("execve");
+	free(arg[0]);
+	free(arg[1]);
+	free(arg[2]);
+	free(arg);
+	close(file);
+	close(fd[1]);
+	return (0);
+}
+
+int	part_2(char **argv, int fd[2])
+{
+	char	**arg2;
+	int		file;
+
+	arg2 = NULL;
+	file = open(argv[4], O_CREAT | O_WRONLY, 0777);
+	if (file == -1)
+	{
+		perror(argv[4]);
+		return (-1);
+	}
+	close(fd[1]);
+	dup2(fd[0], 0);
+	dup2(file, 1);
+	arg2 = alloc(arg2, argv[3]);
+	execve("/bin/bash", arg2, NULL);
+	perror("execve");
+	free(arg2[0]);
+	free(arg2[1]);
+	free(arg2[2]);
+	free(arg2);
+	close(fd[0]);
+	close(file);
+	return (0);
+}
+
+int	main(int argc, char *argv[])
+{
+	int		fd[2];
+	int		pid;
+
+	if (argc == 5)
+	{	
+		if (pipe(fd) == -1)
+		{
+			perror("pipe");
+			return (-1);
+		}
+		pid = fork();
+		if (pid == 0)
+		{
+			if (part_1(argv, fd) == -1)
+				return (-1);
+			return (0);
+		}
+		wait(NULL);
+		if (part_2(argv, fd) == -1)
+			return (-1);
+		close(fd[0]);
+		close(fd[1]);
+	}
 }
